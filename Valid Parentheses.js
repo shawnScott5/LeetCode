83b84be75811e87fa6a4c01b81bccf20e771e967
Shawn Scott <shawnscott5694@gmail.com>
2022-04-23 23:48:09 -0400

    LeetCode Solution

---
+++ Valid Parentheses.js
@@ -0,0 +1,44 @@
+/* Instructions: 
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+ 
+
+Example 1:
+
+Input: s = "()"
+Output: true
+Example 2:
+
+Input: s = "()[]{}"
+Output: true
+Example 3:
+
+Input: s = "(]"
+Output: false
+*/
+
+// Solution:
+const isValid = (s) => {
+    const stack = [];
+    
+    for (let i = 0; i < s.length; i += 1) {
+      const top = stack[stack.length - 1];
+      if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
+        stack.push(s[i]);
+      } else if (s[i] === ')' && top === '(' && stack.length !== 0) {
+        stack.pop();
+      } else if (s[i] === ']' && top === '[' && stack.length !== 0) {
+        stack.pop();
+      } else if (s[i] === '}' && top === '{' && stack.length !== 0) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+    
+    return stack.length === 0;
+  };
